d3c552fd9ababca7fddab5be2051557f4a15ba99
amitr-rapyder <amit.rai@rapyder.com>
2024-10-24 14:37:04 +0530

    changes in fetch url file

---
+++ fetchrul.py
@@ -0,0 +1,59 @@
+import os
+import boto3
+from urllib.parse import urlparse
+from time import time
+
+ddb = boto3.client('dynamodb')
+
+def ensure_http_prefix(true_long_url):
+    """Add http:// prefix if URL doesn't have a protocol"""
+    parsed = urlparse(true_long_url)
+    if not parsed.scheme:
+        return f'http://{true_long_url}'
+    return true_long_url
+
+def update_hit_counter(table_name, short_id, hits):
+    """Update the hit counter and timestamp for the URL"""
+    return ddb.update_item(
+        TableName=table_name,
+        Key={'short_id': {'S': short_id}},
+        UpdateExpression='SET hits = :h, hit_time = :ht',
+        ExpressionAttributeValues={
+            ':h': {'N': str(hits)},
+            ':ht': {'N': str(time())}
+        }
+    )
+
+def lambda_handler(event, context):
+    """AWS Lambda handler for URL shortener redirect service"""
+    try:
+        short_id = event["short_id"]
+        
+        rep = ddb.get_item(
+            Key={'short_id': {'S': short_id}},
+            TableName=os.environ['TABLE_NAME']
+        )
+        
+        true_long_url = ensure_http_prefix(rep['Item']['long_url']['S'])
+        
+        hits = 0
+        if 'hits' in rep['Item']:
+            hits = int(rep['Item']['hits']['N'])
+        hits += 1
+        
+        response_update = update_hit_counter(
+            os.environ['TABLE_NAME'], 
+            short_id, 
+            hits
+        )
+        
+        return {
+            'statusCode': 301,
+            'location': true_long_url
+        }
+        
+    except Exception as e:
+        return {
+            'statusCode': 301,
+            'location': json.dumps({"error": "Not able to find URL"})
+        }
